005365a19477f00d7ddbf398454223cc5a97591e
DESKTOP-V7FTJ2B\Louis Gaillet <louis.jeu2@laposte.net>
2019-02-17 23:21:25 +0100

    scraping not working yet

--- scraper/index.js
+++ scraper/index.js
@@ -5,7 +5,7 @@ var cheerio = require('cheerio');
 var app = express();
 
 
-app.get('/scrape', function(req, res){
+app.get('/naive', function(req, res){
 
     //url of the website we scrape, on this one we have all the needed urls of each hotel
     url = "https://www.relaischateaux.com/fr/site-map/etablissements";
@@ -18,14 +18,37 @@ app.get('/scrape', function(req, res){
             var $ = cheerio.load(html);
             
             //list of the urls we want to get
-            var url;
-            var json = { url };
+            var hotel_url;
+            var name; // name of the hotel 
+            var json = { name, hotel_url };
+
+            //filter to France
+            $('.France').filter(function(){
+                     var data = $(this);
+            });
+            
+            //filter to li section
+            $('.li').filter(function(){
+                var data = $(this);
+                hotel_url = data.children().first().text();
+                json.hotel_url = hotel_url;
+            });
         }
-    })
-})
 
-app.listen('8081')
+        //if there is an error
+        else{
+            console.log('error when requesting website');
+        }
+    });
 
-console.log('Test port 8081');
+    //Save the json results in output.json file
+    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+        console.log('File saved as output.json');
+    });
+});
+
+app.listen('8081');
+
+console.log('Test logs');
 
 exports = module.exports = app;
